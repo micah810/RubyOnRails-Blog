e82393b9cbf686e426ac442ab39dc667eced5309
micah810 <micahlarson810@gmail.com>
2024-08-23 15:56:39 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  #The route below declares that GET /articles requests are mapped
+  # to the index action of ArticlesController.
+  get "/articles", to: "articles#index"
+
+  #The 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

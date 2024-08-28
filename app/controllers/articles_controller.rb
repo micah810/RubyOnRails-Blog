8cf120731274221ce0c6b83f168d449805afab37
micah810 <micahlarson810@gmail.com>
2024-08-28 13:35:46 -0500

    used form builders

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,25 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      # the purpose of the new New Action prepares a blank article for a form
+      # the default of it is to render the form page from app/views/articles/new.html.erb
+      # the setup helps create and save articles/fix errors if something goes wrong
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,31 @@
 Rails.application.routes.draw do
 
   #The root route below is mapped to the index action of ArticleController
-  root "articles#index"
+   #*** root "articles#index"
 
   #The route below declares that GET /articles requests are mapped
   # to the index action of ArticlesController.
-  get "/articles", to: "articles#index"
+  #*** get "/articles", to: "articles#index"
+
+  #The ":ID" designates a route parameter. A route parameter captures a segment of the request's
+  #path, and puts that value into the params Hash,
+  # which is accessible by the controller action.
+  #*** get "/articles/:id", to: "articles#show"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  #*** get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  #** root "posts#index"
+
+
+  #the below routes method "resources" maps all the conventional routs for a collection
+  # of resources, such as articles
+  root "articles#index"
+
+  resources :articles
+  #To inspect what routes are mapped run "bin/rails routes" command
 end
